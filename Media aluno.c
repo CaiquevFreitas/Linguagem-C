7efef87b06cf08c59d414da90c8c36445f6d424a
CaiquevFreitas <caique.v.freitas@ba.estudante.senai.br>
2023-08-20 18:56:25 -0300

    Create Media aluno.c

---
+++ Media aluno.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+    char aluno[50];
+    float teste, prova, quali, trab, media;
+    
+    printf("Digite o nome do aluno: ");
+    scanf("%s", aluno);
+    
+    // Recebendo as notas do aluno
+    printf("Prova: 10, Teste: 6, Trabalho: 3, Qualitativo: 1\n");
+    printf("Digite a nota da prova: ");
+    scanf("%f", &prova);
+    printf("Digite a nota do teste: ");
+    scanf("%f", &teste);
+    printf("Digite a nota do trabalho: ");
+    scanf("%f", &trab);
+    printf("Digite a nota do qualitativo: ");
+    scanf("%f", &quali);
+    
+    //Calculando a média
+    media = (prova+teste+trab+quali)/2;
+    printf("Sua média foi %f", &media);
+    
+    
+}
