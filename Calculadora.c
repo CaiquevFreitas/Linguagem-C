7fc739a0110506f54cbdf654e0582174a0bafede
CaiquevFreitas <caique.v.freitas@ba.estudante.senai.br>
2023-08-25 10:51:50 -0300

    Uptade4 Calculadora.c
    Adicionei um laço de repetição

--- Calculadora.c
+++ Calculadora.c
@@ -2,7 +2,7 @@
 
 int main(){
     float n1, n2;
-    int op;
+    int op = 0;
     
     //Receber os números
     printf("Digite um número: ");
@@ -10,13 +10,16 @@ int main(){
     printf("Digite outro número: ");
     scanf("%f", &n2);
     
-    //Escolhendo a operação
-    printf("Escolha a operação\n");
-    printf("soma=1, subtração=2, multiplicação=3, divisão=4\n");
-    scanf("%d", &op);
-    
-    //Realizando os Calculos
-    switch(op){
+    //Validando operação
+    while (op != 1 && op != 2 && op != 3 && op != 4){
+        
+        //Escolhendo a operação
+        printf("Escolha a operação\n");
+        printf("soma=1, subtração=2, multiplicação=3, divisão=4\n");
+        scanf("%d", &op);
+        
+        //Realizando os Calculos
+        switch(op){
         case 1:
             printf("O resultado da soma é %f", n1 + n2);
             break;
@@ -30,7 +33,9 @@ int main(){
             printf("O resultado da divisão é %f", n1 / n2);
             break;
         default:
-            printf("Operação inválida");
+            printf("Operação inválida\n");
             break;
+    }
+    
     }
 }
