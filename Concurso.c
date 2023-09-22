3e33300612d5ee8ed7978eff8614002e0b9103fc
CaiquevFreitas <caique.v.freitas@ba.estudante.senai.br>
2023-09-22 16:20:29 -0300

    Create Concurso.c

---
+++ Concurso.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+#include <locale.h>
+#include <stdlib.h>
+
+#define TAM 50
+
+struct participantes{
+    int reda;
+    int prova;
+    int total;
+    char nome[TAM];
+};
+
+typedef struct participantes participantes;
+
+int main(){
+    setlocale(LC_ALL,"Portuguese");
+    
+    //Criando e inicializando
+    participantes part[10];
+    int c;
+    
+    //Recebendo dados
+    for(c = 0; c < 10; c++){
+        printf("Informe o nome do %d° participante: ", c+1);
+        scanf("%50.[^\n]s", &part[c].nome);
+        fflush(stdin);
+        
+        printf("Informe a nota da redação: ");
+        scanf("%d", &part[c].reda);
+        fflush(stdin);
+        
+        printf("Informe a nota da prova: ");
+        scanf("%d", &part[c].prova);
+        fflush(stdin);
+        
+        part[c].total = part[c].reda + part[c].prova;
+    }
+}
