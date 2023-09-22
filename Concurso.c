7e6d3e62885c33bdfe092415d4c05b9ffc495c97
Caique Vidal <142412587+CaiquevFreitas@users.noreply.github.com>
2023-09-22 20:08:10 -0300

    Update3 Concurso.c

--- Concurso.c
+++ Concurso.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 
 #define TAM 50
+#define P 5
 
 struct participantes{
     int reda;
@@ -11,18 +12,18 @@ struct participantes{
     int total;
     char nome[TAM];
 };
-
+   
 typedef struct participantes participantes;
 
 int main(){
     setlocale(LC_ALL,"Portuguese");
     
     //Criando e inicializando
-    participantes part[10];
+    participantes part[P];
     int c;
     
     //Recebendo dados
-    for(c = 0; c < 10; c++){
+    for(c = 0; c < P; c++){
         printf("Informe o nome do %d° participante: ", c+1);
         scanf("%50[^\n]s", &part[c].nome);
         fflush(stdin);
@@ -34,7 +35,22 @@ int main(){
         printf("Informe a nota da prova: ");
         scanf("%d", &part[c].prova);
         fflush(stdin);
+        printf("==================================\n");
         
-        part[c].total = part[c].reda + part[c].prova;
+        part[c].total = part[c].reda + part[c].prova;    
     }
-}
+    
+    //Classificando os participantes
+    puts("=====CLASSIFICADOS=====");
+    for(c = 0; c < P; c++){
+    	if(part[c].prova >= 65 && part[c].reda >= 70){
+    		printf("Nome: %s /Redação: %d /Prova: %d /Total: %d\n", part[c].nome, part[c].reda, part[c].prova,part[c].total);
+		}
+	}
+	puts("\n=====DESCLASSIFICADOS=====");
+    for(c = 0; c < P; c++){
+    	if(part[c].prova < 65 || part[c].reda < 70){
+    		printf("Nome: %s /Redação: %d /Prova: %d /Total: %d\n", part[c].nome, part[c].reda, part[c].prova,part[c].total);
+		}
+	}     
+} 
