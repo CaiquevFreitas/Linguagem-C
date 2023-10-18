d2fbf968bd2e11c41a49f0e25ea58a6f3437b4a8
Caique Vidal <142412587+CaiquevFreitas@users.noreply.github.com>
2023-10-18 19:41:52 -0300

    Update Calculadora.c

--- Calculadora.c
+++ Calculadora.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <locale.h>
 
 int main(){
+	setlocale(LC_ALL,"Portuguese");
     float n1, n2;
     int op = 0;
     
@@ -14,8 +16,8 @@ int main(){
     while (op != 1 && op != 2 && op != 3 && op != 4){
         
         //Escolhendo a operação
-        printf("Escolha a operação\n");
-        printf("soma=1, subtração=2, multiplicação=3, divisão=4\n");
+        printf("soma = 1, subtração = 2, multiplicação = 3, divisão = 4\n");
+		printf("Escolha a operação: ");
         scanf("%d", &op);
         
         //Realizando os Calculos
