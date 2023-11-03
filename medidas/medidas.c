265f209b64532db3593e01d26fb140081958a7d3
Caique Vidal <142412587+CaiquevFreitas@users.noreply.github.com>
2023-11-03 19:48:04 -0300

    Create medidas.c

---
+++ medidas/medidas.c
@@ -0,0 +1,47 @@
+#include "medidas.h"
+#include <math.h>
+
+double comprimento(double q, int e, int c){
+	int dist[7] = {0, 1, 2, 3, 4, 5, 6};
+	double result;
+	for(c ; c < 7; c++){
+		if(e >= dist[c]){
+			result = q/pow(10,e - dist[c]);
+			return result;
+		}
+		else if(e < dist[c]){
+			result = q*pow(10,e - dist[c]);
+			return result;	
+		}
+	}	
+}
+
+double volume(double q, int e, int c){
+	int dist[7] = {0, 1, 2, 3, 4, 5, 6};
+	double result;
+	for(c ; c < 7; c++){
+		if(e >= dist[c]){
+			result = q/pow(10,e - dist[c]);
+			return result;
+		}
+		else if(e < dist[c]){
+			result = q*pow(10,e - dist[c]);
+			return result;	
+		}
+	}
+}
+
+double massa(double q, int e, int c){
+	int dist[7] = {0, 1, 2, 3, 4, 5, 6};
+	double result;
+	for(c ; c < 7; c++){
+		if(e >= dist[c]){
+			result = q/pow(10,e - dist[c]);
+			return result;
+		}
+		else if(e < dist[c]){
+			result = q*pow(10,e - dist[c]);
+			return result;	
+		}
+	}
+}
